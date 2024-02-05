0abd99e07123e3176bd9e2ca87e4b02e86a19c4f
Mehboob-Alam1 <ma8122576@gmail.com>
2024-02-05 18:44:16 +0500

    worked on themes and designed the ui screen with success screen and added authentication backed with firebase

---
+++ app/src/main/java/com/mehboob/committemanagement/common/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mehboob.committemanagement.common.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mehboob.committemanagement.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
